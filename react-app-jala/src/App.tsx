7a7ad0a56bd7644429011fe927cee2da8df6f1aa
Pansaleon <rici_chuy@hotmail.com>
2023-10-14 20:04:53 -0600

    primer commit

---
+++ react-app-jala/src/App.tsx
@@ -0,0 +1,6 @@
+import Message from './Message';
+import ListGroup from './components/ListGroup';
+function App() {
+  return <div><Message></Message><ListGroup></ListGroup></div>
+}
+export default App;
---
+++ react-app-jala/src/Message.tsx
@@ -0,0 +1,9 @@
+// PascalCasing
+function Message(){
+    //JSX: Javascript XML
+    let count = 0;
+    
+    return <button><h1>Clicks: {count}</h1></button>;
+}
+
+export default Message;
